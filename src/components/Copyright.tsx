cff2d6c673fee636934d53964ac20e1e66886b75
Hilde Gynoid <hilde.gynoid@gmail.com>
2022-11-24 23:45:13 +0900

    Add GitHub link

--- src/components/Copyright.tsx
+++ src/components/Copyright.tsx
@@ -3,12 +3,17 @@ import { Link, Typography } from '@mui/material';
 
 export default function Copyright() {
   return (
-    <Typography variant="body2" color="text.secondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://hildegynoid.info">
-        Hilde Gynoid
-      </Link>{' '}
-      {new Date().getFullYear()}.
-    </Typography>
+    <>
+      <Typography variant="body2" color="text.secondary" align="center">
+        {'Copyright © '}
+        <Link color="inherit" href="https://hildegynoid.info">
+          Hilde Gynoid
+        </Link>{' '}
+        {new Date().getFullYear()}.
+      </Typography>
+      <Typography variant="body2" color="text.secondary" align="center">
+        <a href="https://github.com/Hilde/cue-converter">Fork on GitHub</a>
+      </Typography>
+    </>
   );
 }
